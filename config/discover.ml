a674449603f6c6a106dd080ef4c0b2658d279d97
cedlemo <cedlemo@gmx.com>
2017-08-15 18:55:17 +0200

    Add default flags for libffi

--- config/discover.ml
+++ config/discover.ml
@@ -14,16 +14,30 @@ let () =
                   "-I/usr/lib/glib-2.0/include"]
       }
     in
+    let default_ffi : C.Pkg_config.package_conf =
+      { libs   = ["-lffi"] ;
+        cflags = ["-O2"; "-Wall"; "-Wextra"; "-Wno-unused-parameter";
+                  "-I/usr/lib/libffi-3.2.1/include";
+                  "-I/usr/include/x86_64-linux-gnu"; (* default ubuntu *)
+                  "-I/usr/include"] (* default ubuntu *)
+      }
+    in
     let conf =
       match C.Pkg_config.get c with
       | None -> default
       | Some pc ->
-          let libffi = Option.value_exn (C.Pkg_config.query pc ~package:"libffi") in
+          let libffi = Option.value (C.Pkg_config.query pc ~package:"libffi") ~default:default_ffi in
           let glib = Option.value (C.Pkg_config.query pc ~package:"glib-2.0") ~default in
           let  module P = C.Pkg_config in
           { libs = libffi.P.libs @ glib.P.libs ;
             cflags = libffi.P.libs @ glib.P.libs }
     in
+    let _ = print_endline "==================== Configurator ================" in
+    let _ = print_endline ">>>>>>>>>>>>>>>>>>>>> libs : <<<<<<<<<<<<<<<<<<<<<<" in
+    let _ = print_endline (Sexp.to_string (sexp_of_list sexp_of_string conf.libs)) in
+    let _ = print_endline ">>>>>>>>>>>>>>>>>>>>> flags : <<<<<<<<<<<<<<<<<<<<<<" in
+    let _ = print_endline (Sexp.to_string (sexp_of_list sexp_of_string conf.cflags)) in
+    let _ = print_endline "==================== Configurator ================" in
 
-    write_sexp "c_flags.sexp"         (sexp_of_list sexp_of_string conf.libs);
-    write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.cflags))
+    write_sexp "c_flags.sexp"         (sexp_of_list sexp_of_string conf.cflags);
+    write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.libs))
