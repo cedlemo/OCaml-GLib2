f601ffc5ad885a2da61f625c5f54a760096d9769
cedlemo <cedlemo@gmx.com>
2017-11-07 18:44:28 +0100

    Update GLib2 raw code bind callable: add Gc.finalise to returned error

--- tests/functions.ml
+++ tests/functions.ml
@@ -62,12 +62,14 @@ let test_filename_to_uri_ok test_ctxt =
 
 let test_filename_to_uri_error test_ctxt =
   let path ="a_totally_bad_path_that_should_not_exist" in
+  let expected = "The pathname “a_totally_bad_path_that_should_not_exist” is \
+                  not an absolute path" in
   match GLib.Core.filename_to_uri path None with
   | Error e -> (
        match e with
                | None -> assert_equal_string "This should not " "have been reached"
                | Some err ->
-                   assert_equal_string "The pathname “a_totally_bad_path_that_should_not_exist” is not an absolute path" Ctypes.(getf (!@ err) GLib.Error.f_message);
+                   assert_equal_string  expected Ctypes.(getf (!@ err) GLib.Error.f_message);
                    assert_equal_int32 (Int32.of_int 5) Ctypes.(getf (!@ err) GLib.Error.f_code)
   )
   | Ok uri -> assert_equal_string "This should not " "have been reached"
