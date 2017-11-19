5c48c08193e66953d8a7d1f74c25a4ab033d32f2
cedlemo <cedlemo@gmx.com>
2017-11-19 18:39:20 +0100

    Remove reference to GObject-Introspection

--- tests/date.ml
+++ tests/date.ml
@@ -1,20 +1,21 @@
 (*
  * Copyright 2017 Cedric LE MOIGNE, cedlemo@gmx.com
- * This file is part of OCaml-GObject-Introspection.
+ * This file is part of OCaml-GLib2.
  *
- * OCaml-GObject-Introspection is free software: you can redistribute it and/or modify
+ * OCaml-GLib2 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * OCaml-GObject-Introspection is distributed in the hope that it will be useful,
+ * OCaml-GLib2 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OCaml-GObject-Introspection.  If not, see <http://www.gnu.org/licenses/>.
+ * along with OCaml-GLib2.  If not, see <http://www.gnu.org/licenses/>.
  *)
+
 open TestUtils
 open OUnit2
 
