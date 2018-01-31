24d589fe7bbf951a8e89b301988ef968a817704e
cedlemo <cedlemo@gmx.com>
2018-01-31 15:38:11 +0100

    Remove useless tests

--- tests/functions.ml
+++ tests/functions.ml
@@ -27,31 +27,6 @@ let test_glib_check_version test_ctxt =
     | None -> assert_equal_string "Ok" "Ok"
     | Some version_mismatch -> assert_equal_string "version mismatch" version_mismatch
 
-let test_ascii_strdown_full test_ctxt =
-  let lowcase = GLib.Core.ascii_strdown "LOW_CASE" (Int64.of_int (-1)) in
-  assert_equal_string "low_case" lowcase
-
-let test_ascii_strdown_partial test_ctxt =
-  let lowcase = GLib.Core.ascii_strdown "LOW_CASE" (Int64.of_int 3) in
-  assert_equal_string "low" lowcase
-
-let test_ascii_strcasecmp test_ctxt =
-  let open Int32 in
-  assert_equal_int32 zero (GLib.Core.ascii_strcasecmp "Hi, Hallo" "Hi, Hallo");
-  assert_equal_int32 zero (GLib.Core.ascii_strcasecmp "hi, hALLO" "Hi, Hallo")
-
-let test_ascii_strncasecmp test_ctxt =
-  let open Int32 in
-  assert_equal_int32 zero (GLib.Core.ascii_strncasecmp "Hi, Hallo" "Hi, Hallo" (Unsigned.UInt64.of_int 8));
-  assert_equal_int32 zero (GLib.Core.ascii_strncasecmp "hi, hALLO" "Hi, Hallo" (Unsigned.UInt64.of_int 8));
-  assert_equal_int32 zero (GLib.Core.ascii_strncasecmp "hi, hALLO" "Hi, Hatoto" (Unsigned.UInt64.of_int 5))
-
-let test_ascii_strup test_ctxt =
-  let upcase = GLib.Core.ascii_strup "up_case" (Int64.of_int (-1)) in
-  assert_equal_string "UP_CASE" upcase;
-  let upcase = GLib.Core.ascii_strup "up_case" (Int64.of_int 3) in
-  assert_equal_string "UP_" upcase
-
 let test_filename_to_uri_ok test_ctxt =
   let path ="/var" in
   match GLib.Core.filename_to_uri path None with
@@ -86,11 +61,6 @@ let tests =
   "GLib functions tests" >:::
     [
       "Test glib check version" >:: test_glib_check_version;
-      "Test glib ascii_strdown full length" >:: test_ascii_strdown_full;
-      "Test glib ascii_strdown partial length" >:: test_ascii_strdown_partial;
-      "Test glib ascii_strcasecmp" >:: test_ascii_strcasecmp;
-      "Test glib ascii_strcasencmp" >:: test_ascii_strncasecmp;
-      "Test glib ascii_strup" >:: test_ascii_strup;
       "Test glib filename_to_uri ok" >:: test_filename_to_uri_ok;
       "Test glib filename_to_uri with error" >:: test_filename_to_uri_error
     ]
