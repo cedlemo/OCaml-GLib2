eda86da5da9422f89c75e428a3e03aa3088de032
cedlemo <cedlemo@gmx.com>
2018-02-10 15:22:56 +0100

    add another test for Time_val functions

--- tests/test_time_val.ml
+++ tests/test_time_val.ml
@@ -29,6 +29,21 @@ let test_time_val_from_iso8601 test_ctxt =
     let _ = assert_equal_int32 (Int32.of_int 2) m in
     assert_equal_int32 (Int32.of_int 07) d
 
+let test_core_get_current_time_and_to_iso8601 test_ctxt =
+  let tv = Ctypes.make GLib.Time_val.t_typ in
+  let tv_ptr = Ctypes.addr tv in
+  let _ = GLib.Core.get_current_time tv_ptr in
+  match GLib.Time_val.to_iso8601 tv_ptr with
+  | None -> assert_equal ~msg:"No iso8601 from Timeval" true false
+  | Some iso8601 -> let now = GLib.Date_time.create_now_local () in
+    let (year, month, day) = GLib.Date_time.get_ymd now in
+    let dt = GLib.Date_time.create_from_timeval_utc tv_ptr in
+    let (y,m,d) = GLib.Date_time.get_ymd dt in
+    let _ = assert_equal_int32 year y in
+    let _ = assert_equal_int32 month m in
+    assert_equal_int32 day d
+
 let tests =
   "GLib Time_val tests" >:::
-    ["Test time_val from iso8601" >:: test_time_val_from_iso8601;]
+    ["Test time_val from iso8601" >:: test_time_val_from_iso8601;
+     "Test Core.get_current_time and time_val to iso8601" >:: test_core_get_current_time_and_to_iso8601;]
