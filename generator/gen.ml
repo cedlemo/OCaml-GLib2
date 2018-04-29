d812ed57d3b19b19a18785967e83788bc06397c0
cedlemo <cedlemo@gmx.com>
2018-04-29 17:14:00 +0200

    Start generate bindings for data structures that are already tested

--- generator/gen.ml
+++ generator/gen.ml
@@ -1,16 +1,7 @@
 module BG = GI_bindings_generator
 module Loader = BG.Loader
 
-(* let print_infos loader =
-  let namespace = Loader.get_namespace loader in
-  let version = Loader.get_version loader in
-  print_endline (">> " ^ namespace);
-  print_endline ("\t - version :" ^ version) *)
-
+let data_structures = ["Error"; "Rand"; "Date"; "DateTime"; "TimeVal"; "TimeZone"]
 
 let () =
-  (* match Loader.load "GLib" () with
-  | Error message -> print_endline "Please check the namespace, something is wrong"
-  | Ok loader ->
-      let _ = print_infos loader in *)
-      Loader.write_bindings_for "GLib" ["Error"]
+      Loader.write_bindings_for "GLib" data_structures
