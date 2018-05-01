bfeb2aa344610db0508dd4d51d1389d097015205
cedlemo <cedlemo@gmx.com>
2018-05-01 12:09:36 +0200

    Core : Add custom constants and function related to the version numbers of GLib

--- lib/Core.ml
+++ lib/Core.ml
@@ -1,1 +1,17 @@
+open Ctypes
+open Foreign
+
 include Core_raw
+
+
+external get_major_version: unit -> int = "get_major_version"
+let c_MAJOR_VERSION = get_major_version () |> Int32.of_int
+
+external get_minor_version: unit -> int = "get_minor_version"
+let c_MINOR_VERSION = get_minor_version () |> Int32.of_int
+
+external get_micro_version: unit -> int = "get_micro_version"
+let c_MICRO_VERSION = get_micro_version () |> Int32.of_int
+
+let check_version =
+  foreign "glib_check_version" (uint32_t @-> uint32_t @-> uint32_t @-> returning (string_opt))
