a9cadcb90bde9b6874c6728480a4470ebc104e18
cedlemo <cedlemo@gmx.com>
2018-05-01 10:14:39 +0200

    Generate constants in Core_raw.ml and include it in Core.ml for easier customization

--- generator/gen.ml
+++ generator/gen.ml
@@ -4,6 +4,7 @@ module Loader = BG.Loader
 let data_structures = ["Error"; "Rand"; "Date"; "DateTime"; "TimeVal"; "TimeZone"]
 
 let () =
-  let _ = Loader.write_constant_bindings_for "GLib" [] [] in
+  let files_suffix = "Raw" in
+  let _ = Loader.write_constant_bindings_for "GLib" ~files_suffix [] [] in
   let _ = Loader.write_enum_and_flag_bindings_for "GLib" in
   Loader.write_bindings_for "GLib" data_structures
