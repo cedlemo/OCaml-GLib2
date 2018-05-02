9f33210b58f8cf232bc3199aa1bc04b2883192b5
cedlemo <cedlemo@gmx.com>
2018-05-02 19:21:07 +0200

    Generator : skip *_VERSION constants, they are implemented manually in lib/Core.mli

--- generator/gen.ml
+++ generator/gen.ml
@@ -3,8 +3,10 @@ module Loader = BG.Loader
 
 let data_structures = ["Error"; "Rand"; "Date"; "DateTime"; "TimeVal"; "TimeZone"]
 
+let constants_to_skip = ["MAJOR_VERSION"; "MINOR_VERSION"; "MICRO_VERSION"]
+
 let () =
   let files_suffix = "Raw" in
-  let _ = Loader.write_constant_bindings_for "GLib" ~files_suffix [] in
+  let _ = Loader.write_constant_bindings_for "GLib" ~files_suffix constants_to_skip in
   let _ = Loader.write_enum_and_flag_bindings_for "GLib" in
   Loader.write_bindings_for "GLib" data_structures
