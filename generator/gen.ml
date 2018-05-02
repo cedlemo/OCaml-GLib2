3d1d20281cd010377a5e8443ba70695f73797e00
cedlemo <cedlemo@gmx.com>
2018-05-02 19:14:25 +0200

    Generator : update to new Loader.write_constant_bindings_for signature

--- generator/gen.ml
+++ generator/gen.ml
@@ -5,6 +5,6 @@ let data_structures = ["Error"; "Rand"; "Date"; "DateTime"; "TimeVal"; "TimeZone
 
 let () =
   let files_suffix = "Raw" in
-  let _ = Loader.write_constant_bindings_for "GLib" ~files_suffix [] [] in
+  let _ = Loader.write_constant_bindings_for "GLib" ~files_suffix [] in
   let _ = Loader.write_enum_and_flag_bindings_for "GLib" in
   Loader.write_bindings_for "GLib" data_structures
