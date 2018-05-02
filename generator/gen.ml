97e520f97a09c9cad59cb5d28add19a1930c03b0
cedlemo <cedlemo@gmx.com>
2018-05-02 19:23:29 +0200

    Generator : fix style issues

--- generator/gen.ml
+++ generator/gen.ml
@@ -1,12 +1,14 @@
 module BG = GI_bindings_generator
 module Loader = BG.Loader
 
+let namespace = "GLib"
+let files_suffix = "Raw"
+
 let data_structures = ["Error"; "Rand"; "Date"; "DateTime"; "TimeVal"; "TimeZone"]
 
-let constants_to_skip = ["MAJOR_VERSION"; "MINOR_VERSION"; "MICRO_VERSION"]
+let const_to_skip = ["MAJOR_VERSION"; "MINOR_VERSION"; "MICRO_VERSION"]
 
 let () =
-  let files_suffix = "Raw" in
-  let _ = Loader.write_constant_bindings_for "GLib" ~files_suffix constants_to_skip in
-  let _ = Loader.write_enum_and_flag_bindings_for "GLib" in
-  Loader.write_bindings_for "GLib" data_structures
+  let _ = Loader.write_constant_bindings_for namespace ~files_suffix const_to_skip in
+  let _ = Loader.write_enum_and_flag_bindings_for namespace in
+  Loader.write_bindings_for namespace data_structures
