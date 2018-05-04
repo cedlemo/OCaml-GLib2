dde54cffd6cb15cef2bdbb092dae0951725c5815
cedlemo <cedlemo@gmx.com>
2018-05-04 20:01:15 +0200

    Generator: generate bindings for some Core functions

--- generator/gen.ml
+++ generator/gen.ml
@@ -2,13 +2,20 @@ module BG = GI_bindings_generator
 module Loader = BG.Loader
 
 let namespace = "GLib"
+
 let files_suffix = "Raw"
 
 let data_structures = ["Error"; "Rand"; "Date"; "DateTime"; "TimeVal"; "TimeZone"]
 
 let const_to_skip = ["MAJOR_VERSION"; "MINOR_VERSION"; "MICRO_VERSION"]
 
+let functions = ["random_double"; "random_double_range";
+                 "random_int"; "random_int_range"]
+
+let sources = Loader.generate_files ("Core" ^ files_suffix)
 let () =
-  let _ = Loader.write_constant_bindings_for namespace ~files_suffix const_to_skip in
+  let _ = Loader.write_constant_bindings_for namespace sources const_to_skip in
+  let _ = Loader.write_function_bindings_for namespace sources functions in
   let _ = Loader.write_enum_and_flag_bindings_for namespace in
-  Loader.write_bindings_for namespace data_structures
+  let _ = Loader.write_bindings_for namespace data_structures in
+  BG.Binding_utils.Sources.close sources
