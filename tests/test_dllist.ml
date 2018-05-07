da8eda464231bc917e9e10ed314ef2607578b1f0
cedlemo <cedlemo@gmx.com>
2018-05-07 09:43:06 +0200

    Dllist tests: improve style

--- tests/test_dllist.ml
+++ tests/test_dllist.ml
@@ -27,17 +27,24 @@ module Int_list =
                     let t_typ = int
                   end)
 
-let build_dllist () =
-  let v = allocate int 1 in
-  let dllist = Int_list.append None v in
-  let v = allocate int 2 in
-  Int_list.append dllist v
-
-let test_list_int_previous_next test_ctxt =
+let test_list_int_append test_ctxt =
   let v = allocate int 1 in
   let dllist = Int_list.append None v in
   let v = allocate int 2 in
   let dllist = Int_list.append dllist v in
+  let length = Int_list.length dllist in
+  assert_equal ~printer:Unsigned.UInt.to_string Unsigned.UInt.(of_int 2) length
+
+let build_dllist () =
+  let v = allocate int 1 in
+  let dllist = Int_list.append None v in
+  let v = allocate int 2 in
+  let dllist = Int_list.append dllist v in
+  let v = allocate int 3 in
+  Int_list.append dllist v
+
+let test_list_int_previous_next test_ctxt =
+  let dllist = build_dllist () in
   let prev = Int_list.previous dllist in
   let _ = match Int_list.data prev with
     | None -> assert true
@@ -52,14 +59,6 @@ let test_list_int_previous_next test_ctxt =
   match Int_list.data prev with
   | None -> assert_failure "The prev node should have data"
   | Some v -> assert_equal_int 1 v
-
-let test_list_int_append test_ctxt =
-  let v = allocate int 1 in
-  let dllist = Int_list.append None v in
-  let v = allocate int 2 in
-  let dllist = Int_list.append dllist v in
-  let length = Int_list.length dllist in
-  assert_equal ~printer:Unsigned.UInt.to_string Unsigned.UInt.(of_int 2) length
 
 let test_list_int_first test_ctxt =
   let dllist = build_dllist () in
@@ -85,13 +84,13 @@ let test_list_int_last test_ctxt =
         let msg = "the data of the last element of the dllist should not be none"
         in assert_equal ~msg false true
       | Some v ->
-          assert_equal ~printer:string_of_int 2 v
+          assert_equal ~printer:string_of_int 3 v
 
 let tests =
   "GLib2 Dl List module tests" >:::
     [
+      "Dl list of int create append length test" >:: test_list_int_append;
       "Dl list of int previous next test" >:: test_list_int_previous_next;
-      "Dl list of int append test" >:: test_list_int_append;
       "Dl list of int first test" >:: test_list_int_first;
       "Dl list of int last test" >:: test_list_int_last;
     ]
