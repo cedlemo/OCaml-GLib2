9c8724c4b6dc022353aff7820b347b030c277799
cedlemo <cedlemo@gmx.com>
2018-05-14 22:14:27 +0200

    SLList : use functor form

--- lib/SLList.ml
+++ lib/SLList.ml
@@ -1,10 +1,21 @@
 open Ctypes
 open Foreign
+(** GLib Singly Linked List
+ * https://developer.gnome.org/glib/stable/glib-Singly-Linked-Lists.html
+*)
 
-type t
-let t_typ : t structure typ = structure "SList"
+module type DataTypes = sig
+  type t
+  type ctype
+  val t_typ : ctype Ctypes.typ
+end
+module Make(Data : DataTypes) = struct
+  type slist
+  let slist : slist structure typ = structure "SList"
+  type data = Data.t
+  let data = Data.t_typ
 
-let f_data = field t_typ "data" (ptr void)
-let f_next = field t_typ "next" (ptr t_typ)
-let _ = seal t_typ
-
+  let glist_data  = field slist "data" (ptr data)
+  let glist_next  = field slist "next" (ptr_opt slist)
+  let () = seal slist
+end
