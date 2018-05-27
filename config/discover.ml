4d1bc4a194cb7714d24c17790642f3e9a8f19882
cedlemo <cedlemo@gmx.com>
2018-05-27 19:44:27 +0200

    discover: remove ouput and clean file

--- config/discover.ml
+++ config/discover.ml
@@ -7,36 +7,38 @@ let write_sexp fn sexp =
 
 let () =
   C.main ~name:"GLib" (fun c ->
-    let default : C.Pkg_config.package_conf =
-      { libs   = ["-lglib-2.0"]
-      ; cflags = ["-O2"; "-Wall"; "-Wextra"; "-Wno-unused-parameter"; "-pthread";
-                  "-I/usr/include/glib-2.0";
-                  "-I/usr/lib/glib-2.0/include"]
-      }
-    in
-    let default_ffi : C.Pkg_config.package_conf =
-      { libs   = ["-lffi"] ;
-        cflags = ["-O2"; "-Wall"; "-Wextra"; "-Wno-unused-parameter";
-                  "-I/usr/lib/libffi-3.2.1/include";
-                  "-I/usr/include/x86_64-linux-gnu"; (* default ubuntu *)
-                  "-I/usr/include"] (* default ubuntu *)
-      }
-    in
-    let conf =
-      match C.Pkg_config.get c with
-      | None -> default
-      | Some pc ->
-          let libffi = Option.value (C.Pkg_config.query pc ~package:"libffi") ~default:default_ffi in
-          let glib = Option.value (C.Pkg_config.query pc ~package:"glib-2.0") ~default in
+      let default : C.Pkg_config.package_conf =
+        { libs   = ["-lglib-2.0"] ;
+          cflags = ["-O2"; "-Wall"; "-Wextra"; "-Wno-unused-parameter";
+                    "-pthread"; "-I/usr/include/glib-2.0";
+                    "-I/usr/lib/glib-2.0/include"]
+        }
+      in
+      let default_ffi : C.Pkg_config.package_conf =
+        { libs   = ["-lffi"] ;
+          cflags = ["-O2"; "-Wall"; "-Wextra"; "-Wno-unused-parameter";
+                    "-I/usr/lib/libffi-3.2.1/include";
+                    "-I/usr/include/x86_64-linux-gnu"; (* default ubuntu *)
+                    "-I/usr/include"]                  (* default ubuntu *)
+        }
+      in
+      let conf =
+        match C.Pkg_config.get c with
+        | None -> default
+        | Some pc ->
+          let query = C.Pkg_config.query pc ~package:"libffi" in
+          let libffi = Option.value query ~default:default_ffi in
+          let query = C.Pkg_config.query pc ~package:"glib-2.0" in
+          let glib = Option.value query ~default in
           let  module P = C.Pkg_config in
-         { libs = (libffi.P.libs @ glib.P.libs) @ ["-L/usr/lib/x86_64-linux-gnu";"-L/usr/lib"];
-           cflags = (libffi.P.cflags @ glib.P.cflags) @ ["-I/usr/include"; "-I/usr/include/x86_64-linux-gnu"] }    in
-    let _ = print_endline "==================== Configurator ================" in
-    let _ = print_endline ">>>>>>>>>>>>>>>>>>>>> libs : <<<<<<<<<<<<<<<<<<<<<<" in
-    let _ = print_endline (Sexp.to_string (sexp_of_list sexp_of_string conf.libs)) in
-    let _ = print_endline ">>>>>>>>>>>>>>>>>>>>> flags : <<<<<<<<<<<<<<<<<<<<<<" in
-    let _ = print_endline (Sexp.to_string (sexp_of_list sexp_of_string conf.cflags)) in
-    let _ = print_endline "==================== Configurator ================" in
-
-    write_sexp "c_flags.sexp"         (sexp_of_list sexp_of_string conf.cflags);
-    write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.libs))
+          { libs =
+              (libffi.P.libs @ glib.P.libs) @
+              ["-L/usr/lib/x86_64-linux-gnu";"-L/usr/lib"];
+            cflags =
+              (libffi.P.cflags @ glib.P.cflags) @
+              ["-I/usr/include"; "-I/usr/include/x86_64-linux-gnu"]
+          }
+      in
+      let list_of_string_to_sexp = sexp_of_list sexp_of_string in
+      write_sexp "c_flags.sexp"         (list_of_string_to_sexp conf.cflags);
+      write_sexp "c_library_flags.sexp" (list_of_string_to_sexp conf.libs))
