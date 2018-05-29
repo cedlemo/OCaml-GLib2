35037808e297c01d2296125f55730da44a6e528c
cedlemo <cedlemo@gmx.com>
2018-05-29 19:39:17 +0200

    DLList test: add test for remove function

--- tests/test_dllist.ml
+++ tests/test_dllist.ml
@@ -84,7 +84,7 @@ let test_list_int_last test_ctxt =
       | Some v ->
           assert_equal ~printer:string_of_int 3 !@v
 
-(* let test_list_int_remove test_ctxt =
+let test_list_int_remove test_ctxt =
   let dllist = build_dllist () in
   let dllist' = Int_list.remove dllist two in
   let length = Int_list.length dllist' in
@@ -93,13 +93,13 @@ let test_list_int_last test_ctxt =
   let last = Int_list.last dllist' in
   let _ = match Int_list.data last with
     | None -> assert_failure "the last element should have some data"
-    | Some d -> assert_equal ~printer:string_of_int 3 d
+    | Some d -> assert_equal ~printer:string_of_int 3 !@d
   in
   let first = Int_list.first dllist' in
   match Int_list.data first with
   | None -> assert_failure "the first element should have some data"
-  | Some d -> assert_equal ~printer:string_of_int 1 d
-*)
+  | Some d -> assert_equal ~printer:string_of_int 1 !@d
+
 let test_list_int_prepend test_ctxt =
   let dllist = Int_list.prepend None one in
   let dllist = Int_list.prepend dllist two in
@@ -181,6 +181,29 @@ let test_list_char_ptr_previous_next test_ctxt =
     let str = GLib.Core.char_ptr_to_string v in
     assert_equal_string str "one"
 
+let test_list_char_ptr_remove test_ctxt =
+  let dllist = Char_ptr_list.append None s_one in
+  let dllist = Char_ptr_list.append dllist s_two in
+  let dllist = Char_ptr_list.append dllist s_three in
+  let dllist' = Char_ptr_list.remove dllist s_two in
+  let length = Char_ptr_list.length dllist' in
+  let _ =
+    Unsigned.(assert_equal ~printer:UInt.to_string UInt.(of_int 2) length) in
+  let last = Char_ptr_list.last dllist' in
+  let _ = match Char_ptr_list.data last with
+    | None -> assert_failure "The next node should have data"
+    | Some v ->
+      let str = GLib.Core.char_ptr_to_string v in
+      assert_equal_string str "three"
+  in
+  let first = Char_ptr_list.first dllist' in
+  match Char_ptr_list.data first with
+    | None -> assert_failure "The next node should have data"
+    | Some v ->
+      let str = GLib.Core.char_ptr_to_string v in
+      assert_equal_string str "one"
+
+
 let tests =
   "GLib2 Dl List module tests" >:::
     [
@@ -188,9 +211,10 @@ let tests =
       "Dl list of int previous next test" >:: test_list_int_previous_next;
       "Dl list of int first test" >:: test_list_int_first;
       "Dl list of int last test" >:: test_list_int_last;
- (*     "Dl list of int remove test" >:: test_list_int_remove; *)
+      "Dl list of int remove test" >:: test_list_int_remove;
       "Dl list of int prepend" >:: test_list_int_prepend;
       "Dl list of int prepend invalid argument" >:: test_list_int_prepend_invalid_argument;
       "Dl list of char ptr create append length test" >:: test_list_char_ptr_append;
       "DL list of char ptr previous next test" >:: test_list_char_ptr_previous_next;
+      "DL list of char remove test" >:: test_list_char_ptr_remove;
     ]
