8010749a82cf4ab9cf59805b19b938da317f8fe4
cedlemo <cedlemo@gmx.com>
2018-06-27 19:54:31 +0200

    Add lib copyright header

--- lib/SLList.ml
+++ lib/SLList.ml
@@ -1,3 +1,21 @@
+(*
+ * Copyright 2018 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of OCaml-GLib2.
+ *
+ * OCaml-GLib2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OCaml-GLib2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCaml-GLib2.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
 open Ctypes
 open Foreign
 (** GLib Singly Linked List
