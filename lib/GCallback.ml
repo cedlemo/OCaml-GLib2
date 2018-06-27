9ec3c316d337a0e398be0ab2b674568338ed678d
cedlemo <cedlemo@gmx.com>
2018-06-27 19:29:32 +0200

    GCallback: add GDestroyNotify functor

--- lib/GCallback.ml
+++ lib/GCallback.ml
@@ -17,3 +17,12 @@ module CompareFunc = struct
     let funptr = funptr f
   end
 end
+
+module GDestroyNotify = struct
+  module Make(Data : DataType) = struct
+    type data = Data.t
+    let data = Data.t_typ
+    let f = ptr data @-> returning void
+    let funptr = funptr f
+  end
+end
