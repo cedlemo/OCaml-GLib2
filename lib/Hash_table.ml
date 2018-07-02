53d870fc5ba97d35633cee5e4452bb93035a96bc
cedlemo <cedlemo@gmx.com>
2018-07-02 20:10:56 +0200

    Hash_table: first implementation of the Hash_table functor

--- lib/Hash_table.ml
+++ lib/Hash_table.ml
@@ -1,6 +1,59 @@
+(*
+ * Copyright 2018 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of OCaml-GLib2.
+ *
+ * OCaml-GLib2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OCaml-GLib2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCaml-GLib2.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
 open Ctypes
 open Foreign
+(*
+ * https://developer.gnome.org/glib/stable/glib-Hash-Tables.html
+ *)
+module type DataTypes = sig
+  type key
+  val key_typ : key Ctypes.typ
+  type value
+  type value_typ : value Ctypes.typ
+  val key_destroy_func : (key ptr -> unit) option
+  val value_destroy_func : (value ptr -> unit) option
+end
 
+module Make(Data : DataTypes) = struct
+  type hash
+  let hash : t structure typ = structure "Hash_table"
+  let () = seal glist
+
+  module Hash_func = struct
+    GCallback.GHashFunc.Make(struct
+      type t = hash
+      let t_typ = hash
+    end)
+  end
+
+  module Key_equal_func = struct
+    GCallback.GEqualFunc.Make(struct
+      type t = hash
+      let t_typ = hash
+    end)
+  end
+
+  let make =
+    foreign "g_hash_table_new" (Hash_func.funptr @-> Key_equal_func.funptr @-> returning hash ptr)
+end
+
+(*
 type t
 let t_typ : t structure typ = structure "Hash_table"
 
@@ -38,3 +91,4 @@ let steal_all =
   foreign "g_hash_table_steal_all" (ptr t_typ @-> returning (void))
 let unref =
   foreign "g_hash_table_unref" (ptr t_typ @-> returning (void))
+   *)
