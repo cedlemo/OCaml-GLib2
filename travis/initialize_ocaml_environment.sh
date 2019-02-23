74c3a46e328d5870941b99d3dc78ff9db128b2ed
Cédric Le Moigne <cedlemo@gmx.com>
2019-02-23 15:28:27 +0100

    Travis: centos set ocaml env variables before using dune

--- travis/initialize_ocaml_environment.sh
+++ travis/initialize_ocaml_environment.sh
@@ -2,7 +2,6 @@
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sudo sh -s /usr/local/bin
 /usr/local/bin/opam init --comp 4.05.0
 opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit memcpy
-. /home/ocaml-glib2/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
 opam pin add --yes gobject-introspection https://github.com/cedlemo/OCaml-GObject-Introspection.git
 opam pin add --yes gi-bindings-generator https://github.com/cedlemo/OCaml-GI-ctypes-bindings-generator.git
 
