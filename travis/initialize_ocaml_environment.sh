ba53a47e8cb311581c5107337e485e97e6531915
Cédric Le Moigne <cedlemo@gmx.com>
2019-02-24 09:59:32 +0100

    Travis: centos 7 - use installed switch for ocaml 4.05

--- travis/initialize_ocaml_environment.sh
+++ travis/initialize_ocaml_environment.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
-opam switch create 4.05.0 ocaml-base-compiler.4.05.0
-eval $(opam env)
 opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit memcpy
 opam pin add --yes gobject-introspection https://github.com/cedlemo/OCaml-GObject-Introspection.git
 opam pin add --yes gi-bindings-generator https://github.com/cedlemo/OCaml-GI-ctypes-bindings-generator.git
