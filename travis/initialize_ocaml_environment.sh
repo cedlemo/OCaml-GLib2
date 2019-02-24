dfe4ebcb5ab631c78c1db0ae8befcf30dc1a62e7
Cédric Le Moigne <cedlemo@gmx.com>
2019-02-24 09:07:08 +0100

    Travis: force ocaml version 4.05.0

--- travis/initialize_ocaml_environment.sh
+++ travis/initialize_ocaml_environment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
-/usr/local/bin/opam init --comp 4.05.0
+opam switch create 4.05.0 ocaml-base-compiler.4.05.0
+eval $(opam env)
 opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit memcpy
 opam pin add --yes gobject-introspection https://github.com/cedlemo/OCaml-GObject-Introspection.git
 opam pin add --yes gi-bindings-generator https://github.com/cedlemo/OCaml-GI-ctypes-bindings-generator.git
