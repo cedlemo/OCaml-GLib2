4f1777d969d4b9ace4cd629ea390e2d50db464a5
cedlemo <cedlemo@gmx.com>
2019-12-31 18:01:08 +0100

    Travis: update and upgrade opam packages

--- travis/initialize_ocaml_environment.sh
+++ travis/initialize_ocaml_environment.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+opam update
+opam upgrade -y
 opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit memcpy gobject-introspection
 opam pin add --yes gi-bindings-generator https://github.com/cedlemo/OCaml-GI-ctypes-bindings-generator.git
 
