9fde8b19f861f32c59f3cb4c2e6de05ae742d75f
cedlemo <cedlemo@gmx.com>
2019-12-31 16:17:34 +0100

    travis: gobject-introspection is now published, no need to pin it

--- travis/initialize_ocaml_environment.sh
+++ travis/initialize_ocaml_environment.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit memcpy
-opam pin add --yes gobject-introspection https://github.com/cedlemo/OCaml-GObject-Introspection.git
+opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit memcpy gobject-introspection
 opam pin add --yes gi-bindings-generator https://github.com/cedlemo/OCaml-GI-ctypes-bindings-generator.git
 
