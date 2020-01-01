89345e34f2989c855e3587819bbb169a22d94486
cedlemo <cedlemo@gmx.com>
2020-01-01 10:17:33 +0100

    Travis: pin packages dependencies

--- travis/initialize_ocaml_environment.sh
+++ travis/initialize_ocaml_environment.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 opam update
 opam upgrade -y
-opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit memcpy gobject-introspection
+opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit memcpy
+opam pin add --yes conf-gobject-introspection https://github.com/cedlemo/conf-gobject-introspection.git
+opam pin add --yes gobject-introspection https://github.com/cedlemo/OCaml-GObject-Introspection.git
 opam pin add --yes gi-bindings-generator https://github.com/cedlemo/OCaml-GI-ctypes-bindings-generator.git
 
